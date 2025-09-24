5e6353d083242f6e60913581a87beb868add4134
Jeffrey H. Johnson <trnsz@pobox.com>
2025-09-24 03:29:36 -0400

    Only flock on Linux and give sane error messages
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: all clean
 all:
-	@./run.sh
+	@./run.sh; \
+	  status=$$?; if [ "$${status:-}" -eq 14 ]; then { printf '%s\n' "flock(1) error - is another instance running?"; exit 1; }; fi
+
 clean:
-	@./clean.sh
+	@./clean.sh; \
+	  status=$$?; if [ "$${status:-}" -eq 14 ]; then { printf '%s\n' "flock(1) error - is another instance running?"; exit 1; }; fi
--- clean.sh
+++ clean.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env sh
+
+if [ "Linux" = "$(uname -s 2> /dev/null || :)" ]; then
+  # shellcheck disable=SC2015
+  command -v flock > /dev/null 2>&1 && {
+    # shellcheck disable=SC2015,SC2154
+    [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -E 14 -en "$0" "$0" "$@" || :
+  } || :
+fi
+
 rm -f ./a.out ./errnum ./*.sfd ./*.ttf ./*.log ./core-* ./*.core ./core ./clean ./convert
