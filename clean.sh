5d508be4bc252a0c12c330d21ea1258f7b44e415
Jeffrey H. Johnson <trnsz@pobox.com>
2025-09-24 05:56:45 -0400

    Add csh guard to clean.sh
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- clean.sh
+++ clean.sh
@@ -4,6 +4,11 @@
 # Copyright (c) 2025 The DPS8M Development Team
 # scspell-id: 58d56a76-9924-11f0-9b08-80ee73e9b8e7
 
+# shellcheck disable=SC2006,SC2046,SC2065,SC2116
+test _`echo asdf 2>/dev/null` != _asdf >/dev/null &&\
+  printf '%s\n' "FATAL: Using csh as sh is not supported." &&\
+  exit 1
+
 if [ "Linux" = "$(uname -s 2> /dev/null || :)" ]; then
   # shellcheck disable=SC2015
   command -v flock > /dev/null 2>&1 && {
