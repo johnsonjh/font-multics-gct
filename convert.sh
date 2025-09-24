1eef453661747c161db3edf841dc76e2e66cc627
Jeffrey H. Johnson <trnsz@pobox.com>
2025-09-23 22:20:13 -0400

    Update .gitignore; appease ShellCheck
    Signed-off-by: Jeffrey H. Johnson <trnsz@pobox.com>

--- convert.sh
+++ convert.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-cc -o errnum errnum.c
+test -x ./errnum || {
+  cc -o errnum errnum.c || {
+    printf '%s\n' "FATAL: errnum.c compilation failed"
+    exit 1
+  }
+}
 
 for gct_file in gct_*_; do
   if [ -f "${gct_file:?}" ]; then
@@ -14,15 +19,15 @@ for gct_file in gct_*_; do
     printf '%s\n' "STROKE" "======" "" >> "${sfd_name:?}.log"
     ./convert_gct_stroke.py "${gct_file:?}" "${sfd_name:?}_Stroke.sfd" >> "${sfd_name:?}.log" 2>&1
     _E=$?
-    ./errnum ${_E:?} >> "${sfd_name:?}.log" 2>&1
-    printf '%s' "$(./errnum "${_E:?}")"
+    ./errnum "${_E:?}" >> "${sfd_name:?}.log" 2>&1
+    printf '%s' "$(./errnum "${_E:?}" || :)"
 
     printf '\t %s' "Outline: "
     printf '%s\n' "" "OUTLINE" "=======" "" >> "${sfd_name:?}.log"
     ./convert_gct_outline.py "${gct_file:?}" "${sfd_name:?}.sfd" >> "${sfd_name:?}.log" 2>&1
     _E=$?
-    ./errnum ${_E:?} >> "${sfd_name:?}.log" 2>&1
-    printf '%s' "$(./errnum "${_E:?}")"
+    ./errnum "${_E:?}" >> "${sfd_name:?}.log" 2>&1
+    printf '%s' "$(./errnum "${_E:?}" || :)"
 
     test -f "${sfd_name:?}.sfd" && {
       printf '\t%s' "TrueType: "
@@ -30,8 +35,8 @@ for gct_file in gct_*_; do
       # shellcheck disable=SC2016
       fontforge -lang=ff -c 'Open($1); Generate($2)' "${sfd_name:?}.sfd" "${sfd_name:?}.ttf" >> "${sfd_name:?}.log" 2>&1
       _E=$?
-      ./errnum ${_E:?} >> "${sfd_name:?}.log" 2>&1
-      printf '%s' "$(./errnum "${_E:?}")"
+      ./errnum "${_E:?}" >> "${sfd_name:?}.log" 2>&1
+      printf '%s' "$(./errnum "${_E:?}" || :)"
     }
 
     printf '%s\n' ""
